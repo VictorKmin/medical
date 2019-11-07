2c165e373ab4685702879023b40609fd39172e29
Viktor <viktor.kmin@eleks.com>
2019-11-07 19:31:25 +0200

    Add check admin middleware. Add block\unblock user possibility

--- src/routers/admin/admin.router.ts
+++ src/routers/admin/admin.router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
-import { checkIsAdmin, checkIsUserExists } from '../../middlewares';
 
+import { checkIsAdmin, checkIsUserExists } from '../../middlewares';
 import { adminController } from '.././../controllers';
 
 const router = Router();
